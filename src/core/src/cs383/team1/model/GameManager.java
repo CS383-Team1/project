28f3a1c4b8f74691903f6c805611ce4c8ab22a11
well3112 <lance.a.wells@gmail.com>
2015-04-04 13:07:20 -0700

    "Fixed" code and updated to linux kernel standards
    - Changed changeLevel methods to output int values rather than boolean.
    - Shortened and altered the section that checks for a stairsEntity.
    - Updated formatting to match Linux Kernel standards.

--- src/core/src/cs383/team1/model/GameManager.java
+++ src/core/src/cs383/team1/model/GameManager.java
@@ -53,8 +53,8 @@ public final class GameManager {
 		Player player;
 		Position next;
 		Tile target;
-                //This object is, like, super gross and should be removed once some
-                //super smart, awesome person comes up with an alternative solution (well3112)
+                /*This object is, like, super gross and should be removed once some
+                super smart, awesome person comes up with an alternative solution (well3112)*/
                 StairsEntity se;
 
 
@@ -95,31 +95,16 @@ public final class GameManager {
 				player.pos = next;
 			}
                         
-                        //Check if the current/targeted tile is a stairs tile (well3112)
-                        if(target.type() == 3){
-                                //Iterate through all entities (well3112)
-                                for (Entity e : areas.current.entities) {
-                                    if(
-                                            //Find a stairs entity at the current
-                                            //position of the player (well3112)
-                                            e.pos().x == player.pos().x &&
-                                            e.pos().y == player.pos().y &&
-                                            e.type() == 2)
-                                    {
-                                            //Typecast the discovered entity into
-                                            //a stairsentity object (well3112)
-                                            se = (StairsEntity) e;
-                                            //Change the area and display an
-                                            //error if the transition is invalid (well3112)
-                                            if (!changeArea(
-                                                    se.destination(),
-                                                    se.destinationPos()))
-                                                    Gdx.app.error(
-                                                            "GameManager:update",
-                                                            "invalid stairs transition");
+                        //Check if the targeted tile contains a stairs entity (well3112)
+                        for (Entity e : areas.current.entities) {
+                                if(
+                                        e.pos().x == player.pos().x &&
+                                        e.pos().y == player.pos().y
+                                        && e.type() == 2) {
+                                        se = (StairsEntity) e;
+                                        changeArea(se.destination(),se.destinationPos());
                                     }
                                 }
-                        }
 		}
 
 		/* TODO: move the keyhandling code to the StateManager */
@@ -130,38 +115,28 @@ public final class GameManager {
 	}
         
         //Method to change the current area and set the future position (well3112)
-        public boolean changeArea(String s, Position pos)
+        public int changeArea(String s, Position pos)
         {
-            //Need support/major changes for multiplayer?
-            //Each level currently uses baked-in character data
-            //May need to change this to take a player parameter as well (well3112)
+            /*Need support/major changes for multiplayer?
+            Each level currently uses baked-in character data
+            May need to change this to take a player parameter as well (well3112)*/
             
-            //Check if the changearea method was successful (well3112)
-            if(changeArea(s))
-            {
-                    //Add the appropriate level transition formatting 
-                    //and set the player position (well3112)
+            if(changeArea(s) == 0) {
                     areas.areas.get("area/".concat(s.concat(".txt"))).player.pos = pos;
-                    //The function was successful, return true (well3112)
-                    return true;
+                    return 0;
             }
-            //Level doesn't exist, return false (well3112)
-            return false;
+            return -1;
         }
         
         //Method to change the current area (well3112)
-        public boolean changeArea(String s)
+        public int changeArea(String s)
         {
-            //Add the appropriate level transition formatting (well3112)
             String sFull = "area/".concat(s.concat(".txt"));
-            //Check if the level to transition to exists (well3112)
-            if(areas.areas.containsKey(sFull))
-            {
-                    //Change the current area and return true (well3112)
+            if(areas.areas.containsKey(sFull)) {
                     areas.current = sFull != null ? areas.areas.get(sFull) : null;
-                    return true;
+                    return 0;
             }
-            //Level doesn't exist, return false (well3112)
-            return false;
+            Gdx.app.error("GameManager:update","invalid stairs transition");
+            return -1;
         }
 }
