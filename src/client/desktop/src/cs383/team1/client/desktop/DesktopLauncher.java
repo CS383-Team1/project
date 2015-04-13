d79a0bb8adaaad3dab9214252ce5fa5df6d76c5a
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-04-12 22:58:15 -0700

    created gdx projects

---
+++ src/client/desktop/src/cs383/team1/client/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cs383.team1.client.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cs383.team1.client.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
---
+++ src/server/desktop/src/cs383/team1/server/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cs383.team1.server.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cs383.team1.server.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
