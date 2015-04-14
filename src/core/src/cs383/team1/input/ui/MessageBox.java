54c2df2d3929d09f2e244b59c8e3d7494764f68f
well3112 <lance.a.wells@gmail.com>
2015-04-14 01:45:21 -0700

    Slight skin change
    - Small comments additions
    - Slight skin change

--- src/core/src/cs383/team1/input/ui/MessageBox.java
+++ src/core/src/cs383/team1/input/ui/MessageBox.java
@@ -1,18 +1,12 @@
 package cs383.team1.input.ui;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.SplitPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import java.util.ArrayList;
 
 /**
@@ -20,24 +14,24 @@ import java.util.ArrayList;
  * @author Lance
  */
 public class MessageBox {
-        private ArrayList <String> text;
-        private Label mText;
-        private Table mTable;
-        private ScrollPane mScroll;
-        private SplitPane mHistory;
-        private Table bar;
+        private final ArrayList <String> text;
+        private final Label mText;
+        private final Table mTable;
+        private final ScrollPane mScroll;
+        private final SplitPane mHistory;
+        private final Table bar;
         
-        private SplitPane msg;
+        private final SplitPane msg;
         public TextField input;
         
         public MessageBox(Skin sk)
         {
                 text = new ArrayList();
-
                 mTable = new Table(sk);
                 mText = new Label("",sk);
                 input = new TextField("", sk);
                 
+                //Create the "Chat History" section
                 mScroll = new ScrollPane(mTable, sk, "chat");
                 mTable.pad(10);
                 mTable.padBottom(15);
@@ -47,24 +41,23 @@ public class MessageBox {
                 mScroll.setOverscroll(false, false);
                 mScroll.setSmoothScrolling(false);
                 
+                //Add the top bar to the chat window
                 bar = new Table();
                 bar.add(SubMenu.getImage("bar")).fillX();
-                
                 mHistory = new SplitPane(bar, mScroll, true, sk);
                 bar.setFillParent(true);
                 mHistory.setSplitAmount   ((float) 0.2500);
                 mHistory.setMaxSplitAmount((float) 0.2500);
                 mHistory.setMinSplitAmount((float) 0.2499);
                 
-//                msg = new SplitPane(mScroll, input, true, sk);
+                //Create the overall window's splitpane
                 msg = new SplitPane(mHistory, input, true, sk);
 
                 msg.setWidth(Gdx.graphics.getWidth());
                 msg.setHeight(Gdx.graphics.getWidth()/6);
                 msg.setX(0);
                 msg.setY(0);
-                
-                System.out.println((float)Gdx.graphics.getWidth()/42);
+
                 msg.setSplitAmount   ((float) 0.8500);
                 msg.setMaxSplitAmount((float) 0.8500);
                 msg.setMinSplitAmount((float) 0.8499);
@@ -74,6 +67,7 @@ public class MessageBox {
                 mTable.left().bottom().add(mText).width(msg.getWidth());
         }
         
+        //Return the overall pane for the UIDisplay
         public SplitPane msg(){
                 return msg;
         }
@@ -86,6 +80,8 @@ public class MessageBox {
         }
         
         //Update messages list
+        //This should be updated so it doesn't have to reconstruct the entire
+        //chat history each time
         public void updateMessages()
         {
                 String s = "";
