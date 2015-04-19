282294561fac1251469e3ccdcc02fefab9274815
well3112 <lance.a.wells@gmail.com>
2015-04-18 18:45:59 -0700

    UIListener Refactoring
    - Fixed the mess of code that was the UI KeyListener implementation

--- src/core/src/cs383/team1/render/UIDisplay.java
+++ src/core/src/cs383/team1/render/UIDisplay.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import cs383.team1.input.ui.InteractionMenu;
 import cs383.team1.input.ui.MainMenu;
 import cs383.team1.input.ui.MessageBox;
+import cs383.team1.input.ui.UIListener;
 import cs383.team1.model.GameManager;
 import cs383.team1.model.overworld.Area;
 import cs383.team1.model.overworld.AreaManager;
@@ -26,6 +27,7 @@ public class UIDisplay extends Display{
         MessageBox msg;
         MainMenu menu;
         InteractionMenu interact;
+        UIListener uiListen;
         
         final Player player = GameManager.instance.areas.current.player;
         final AreaManager areas = GameManager.instance.areas;
@@ -58,6 +60,8 @@ public class UIDisplay extends Display{
                 msg = new MessageBox(skin);
                 menu = new MainMenu(skin);
                 interact = new InteractionMenu(skin);
+                
+                uiListen = new UIListener(stage, player, menu, msg, interact);
 
                 stage.addActor(msg.msg());
                 stage.addActor(menu.menu());
@@ -66,52 +70,54 @@ public class UIDisplay extends Display{
                 menu.menu().setVisible(false);
                 interact.interact().setVisible(false);
                 
-                stage.addListener(new InputListener() {
-                        @Override
-                        public boolean keyDown( InputEvent event, int keyCode ) {
-                                if (keyCode == Input.Keys.ENTER) {
-                                        if (!msg.input.equals("") && stage.getKeyboardFocus() == msg.input) {
-                                                msg.addMessage(msg.input.getText());
-                                                msg.input.setText("");
-                                                stage.setKeyboardFocus(null);
-                                                return true;
-                                        } else {
-                                                stage.setKeyboardFocus(msg.input);
-                                        }
-                                        
-                                } else if ( keyCode == Input.Keys.ESCAPE && menu.menu().isVisible() ) {
-                                        menu.menu().setVisible(false);
-                                        return true;
-                                } else if ( keyCode == Input.Keys.ESCAPE ) {
-                                        menu.menu().setVisible(true);
-                                        return true;
-                                        // && keyboard focus != messagebox
-                                } else if (keyCode == Input.Keys.SPACE && stage.getKeyboardFocus() != msg.input) {
-                                        if (player.facing == 0)
-                                                useMenu(new Position(player.pos.x, player.pos.y + 1));
-                                        else if (player.facing == 1)
-                                                useMenu(new Position(player.pos.x + 1, player.pos.y));
-                                        else if (player.facing == 2)
-                                                useMenu(new Position(player.pos.x, player.pos.y - 1));
-                                        else if (player.facing == 3)
-                                                useMenu(new Position(player.pos.x - 1, player.pos.y));
-                                        return true;
-                                } else if (keyCode == Input.Keys.DOWN) {
-                                        interact.getNext();
-                                        return (interact.interact.isVisible());
-                                } else if (keyCode == Input.Keys.UP) {
-                                        interact.getPrevious();
-                                        return (interact.interact.isVisible());
-                                } else if(keyCode == Input.Keys.LEFT || keyCode == Input.Keys.RIGHT) {
-                                        return (interact.interact.isVisible());
-                                }
-                                return false;
-                        }
-                });
+                stage.addListener(uiListen);
+                
+//                stage.addListener(new InputListener() {
+//                        @Override
+//                        public boolean keyDown( InputEvent event, int keyCode ) {
+//                                if (keyCode == Input.Keys.ENTER) {
+//                                        if (!msg.input.equals("") && stage.getKeyboardFocus() == msg.input) {
+//                                                msg.addMessage(msg.input.getText());
+//                                                msg.input.setText("");
+//                                                stage.setKeyboardFocus(null);
+//                                                return true;
+//                                        } else {
+//                                                stage.setKeyboardFocus(msg.input);
+//                                        }
+//                                        
+//                                } else if ( keyCode == Input.Keys.ESCAPE && menu.menu().isVisible() ) {
+//                                        menu.menu().setVisible(false);
+//                                        return true;
+//                                } else if ( keyCode == Input.Keys.ESCAPE ) {
+//                                        menu.menu().setVisible(true);
+//                                        return true;
+//                                        // && keyboard focus != messagebox
+//                                } else if (keyCode == Input.Keys.SPACE && stage.getKeyboardFocus() != msg.input) {
+//                                        if (player.facing == 0)
+//                                                useMenu(new Position(player.pos.x, player.pos.y + 1));
+//                                        else if (player.facing == 1)
+//                                                useMenu(new Position(player.pos.x + 1, player.pos.y));
+//                                        else if (player.facing == 2)
+//                                                useMenu(new Position(player.pos.x, player.pos.y - 1));
+//                                        else if (player.facing == 3)
+//                                                useMenu(new Position(player.pos.x - 1, player.pos.y));
+//                                        return true;
+//                                } else if (keyCode == Input.Keys.DOWN) {
+//                                        interact.getNext();
+//                                        return (interact.interact.isVisible());
+//                                } else if (keyCode == Input.Keys.UP) {
+//                                        interact.getPrevious();
+//                                        return (interact.interact.isVisible());
+//                                } else if(keyCode == Input.Keys.LEFT || keyCode == Input.Keys.RIGHT) {
+//                                        return (interact.interact.isVisible());
+//                                }
+//                                return false;
+//                        }
+//                });
         }
         
-        private void useMenu(Position p)
-        {
-                interact.useMenu(areas.findEntity(p), player.facing);
-        }
+//        private void useMenu(Position p)
+//        {
+//                interact.useMenu(areas.findEntity(p), player.facing);
+//        }
 }
